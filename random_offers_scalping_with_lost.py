8318efea30184cfc531a95bbc3eb4d03c75ff05f
alfacentavra <49415297+morefinances@users.noreply.github.com>
2024-01-11 16:10:57 +0300

    Add files via upload

---
+++ random_offers_scalping_with_lost.py
@@ -0,0 +1,61 @@
+import pandas as pd
+import random as rnd
+from time import sleep
+
+# количество заявок 
+N = 305
+
+ind = 1
+inditer = 0
+
+# считываем тикеры
+file = "C:\\files\\tickers_for_work.csv"
+offers = pd.read_csv(file, delimiter=';')
+
+uniq_offers = []
+
+# удаляем дублирование тикеров
+for i in range(len(offers.columns)):
+    if "." not in offers.columns[i]:
+        uniq_offers.append(offers.columns[i])
+
+# генерация рандомного пула заявок по полученным тикерам
+while ind <= N:
+    print(f"index = {ind}", end = ' ')
+    timeout = rnd.randint(1,3)
+    print(f"timeout = {timeout/10}")
+    numberorders = rnd.randint(8,10)
+    print(f"number_orders={numberorders}")
+    A = []
+    B = []
+    C = []
+    D = []
+    for i in range(numberorders):
+        if ind % 100 != 0:
+            A.append(ind)
+            direction = rnd.randint(1,2)
+            if direction == 1:
+                B.append("Buy")
+            else:
+                B.append("Sell")
+            C.append(10 * rnd.randint(1,9))
+            D.append(rnd.choice(uniq_offers))  
+        df=pd.DataFrame({
+            'id': A,
+            'direct': B,
+            'size': C,
+            'tiker': D
+        })  
+        ind += 1
+    print(df)
+    df.to_csv("C:/files/offers.csv", sep=" ", index = False, header = False)
+    sleep(timeout/100)
+    df.to_csv("C:/files/historyoffers.csv", mode='a', sep=" ", index = False, header = False)
+    sleep(timeout/100)
+    inditer += 1
+
+print('<-- все заявки отправлены -->')
+
+sleep(timeout/10)
+df=pd.DataFrame({})
+df.to_csv("C:/files/offers.csv", sep=" ", index = False, header = False)
